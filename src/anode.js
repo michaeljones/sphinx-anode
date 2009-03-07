c3701bcb2a9a5978f5c1667237d8bb0e22091d0a
mpj <mpj@dneg.com>
2009-03-07 17:09:35 +0000

    Progressive updates to the helper page.

--- src/anode.js
+++ src/anode.js
@@ -15,9 +15,22 @@ function Entry( div )
 	this.div = div;
 }
 
+Entry.prototype.output = function()
+{
+	var output = "&nbsp;&nbsp;&nbsp;&nbsp;.. annotation:: " 
+					+ this.div.top + " "
+					+ this.div.left + " "
+					+ this.div.height + " "
+					+ this.div.width;
+
+	return output;
+}
+
 function State()
 {
-	this.entries = []
+	this.image = "";
+	this.entries = [];
+	this.live = false;
 }
 
 State.prototype.new_div = function()
@@ -25,26 +38,79 @@ State.prototype.new_div = function()
 	return "new_div" + this.entries.length;
 }
 
+State.prototype.set_image = function( src )
+{
+	this.image = Image();
+	this.image.src = src;
+}
+
+State.prototype.output = function()
+{
+	var output = ".. annotated-image:: " 
+						+ this.image.height + " "
+						+ this.image.width + " "
+						+ this.image.src;
+
+	for ( var i=0; i<this.entries.length; ++i )
+	{
+		output += "<br/>";
+		output += this.entries[i].output();
+	}
+
+	return output;
+}
+
+
 state = new State();
+
+
+function build_output()
+{
+	var output_text = state.output()
+
+	var output = $("#output");
+
+	output.html(output_text);
+}
+
 
 function image_mouse_down( info )
 {
 	var div_name = state.new_div();
 
+	state.live = true;
+
 	$("#imagebox").append("<div class='area' id='" + div_name + "'></div>");
 
-	state.entries.push( new Entry( new Div( $("#" + div_name ), div_name, info.pageY - this.offsetTop, info.pageX - this.offsetLeft ) ) );
+	state.entries.push(
+			new Entry(
+				new Div(
+					$("#" + div_name ),
+					div_name,
+					info.pageY - this.offsetTop,
+					info.pageX - this.offsetLeft
+					)
+				)
+			);
 
-	// $("#entries").html( info.pageX + " " + info.pageY + " <br/>" + this.offsetLeft + " " + this.offsetTop + "<br/>Id: " + div_name );
-}
-
-function image_mouse_up( info )
-{
 	var entry = state.entries[ state.entries.length - 1 ];
 	var div = entry.div;
 
-	div.height = ( info.pageY ) - div.top;
-	div.width = ( info.pageX ) - div.left;
+	div.div.css("position", "absolute");
+	div.div.css("top", div.top + "px" );
+	div.div.css("left", div.left + "px" );
+}
+
+
+function image_mouse_up( info )
+{
+	state.live = false;
+
+	var entry = state.entries[ state.entries.length - 1 ];
+	var div = entry.div;
+
+	div.height = ( info.pageY - this.offsetTop ) - div.top;
+	div.width = ( info.pageX - this.offsetLeft ) - div.left;
 
 	if ( div.height < 1 || div.width < 1 )
 	{
@@ -53,29 +119,40 @@ function image_mouse_up( info )
 		return;
 	}
 
-	// $("#output").html( div.top + " " + div.left + "<br/> " + div.height + " " + div.width + " <br/> " + this.offsetTop + " " + this.offsetLeft + "<br/>Length: " + state.divs.length + "<br/>Id: " +  div.name );
-
+	div.div.css("position", "absolute");
 	div.div.css("top", div.top + "px" );
 	div.div.css("left", div.left + "px" );
 	div.div.css("height", div.height + "px" );
 	div.div.css("width", div.width + "px" );
-	div.div.css("position", "absolute");
+
+	entry.name_id = "name_" + state.entries.length;
+	entry.desc_id = "desc_" + state.entries.length;
 
 	var form = $("#entries form");
 
 	form
 		.append("<div>")
-		.append("Name: <input type='text'/>")
-	    .append("Description: <textarea></textarea>")
+		.append("<div>Name: <input id='" + entry.name_id + "' type='text'/></div>")
+	    .append("<div>Description: <textarea id='" + entry.desc_id + "' ></textarea></div>")
 	    .append("</div>");
+
+	build_output();
 }
+
 
 function image_mouse_move( info )
 {
+	if ( ! state.live ) return;
+
+	var entry = state.entries[ state.entries.length - 1 ];
+	var div = entry.div;
+
+	div.height = ( info.pageY - this.offsetTop ) - div.top;
+	div.width = ( info.pageX - this.offsetLeft ) - div.left;
+
+	div.div.css("height", div.height + "px" );
+	div.div.css("width", div.width + "px" );
 }
-
-
-
 
 
 function image_key_callback( key ) 
@@ -85,11 +162,10 @@ function image_key_callback( key )
 
 	var imagesrc = $("#imagename").val();
 
-	var image = Image();
-	image.src = imagesrc;
+	state.set_image( imagesrc );
 
-	var height = image.height;
-	var width = image.width;
+	var height = state.image.height;
+	var width = state.image.width;
 
 	var imagebox = $("#imagebox");
 
@@ -100,9 +176,10 @@ function image_key_callback( key )
 
 	imagebox.mousedown( image_mouse_down );
 	imagebox.mouseup( image_mouse_up );
-	// imagebox.mousemove( image_mouse_move );
+	imagebox.mousemove( image_mouse_move );
 
 }
+
 
 function main()
 {
